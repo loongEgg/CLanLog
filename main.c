661364cfdd4f6c4c407cd9615fa2937363771e89
loongegg <loongegg@163.com>
2020-09-11 10:46:36 +0800

    6.0- A  2020-09-11  最初版本, 简单单文件编译

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+ *         file: main.c
+ *  description: Linux下C语言彩色控制台编程实践_基于gcc,gdb,VSCode和WSL_Ubuntu_18.04
+ *       author: B站/微信公众 香辣恐龙蛋（从公众号可以获取各期的笔记和源代码）
+ *         date: 2020-09-11
+ *      version: 6.0
+ * modification:
+ *               major  minor  date        brief
+ *                 6.0      A  2020-09-11  最初版本, 简单单文件编译
+ *       remark:
+ *               0. 在linux中运行程序 ./test
+ *               1. 1.0A版本的编译指令 gcc main.c -o test
+ *                  把文件编译到一个叫做test的二进制可执行文件
+ */
+
+/*
+ *     function: int main(void)
+ *   desription: 简单单文件编译
+ *       author: B站/微信公众 香辣恐龙蛋
+ *         date: 2020-09-11
+ *        input: 
+ *       output: 
+ *      version: 6.0
+ * modification:
+ *               major      minor       date        brief
+ *                 6.0          A       2020-09-11  简单单文件编译
+ */
+int main(void)
+{
+  printf("hello world");
+  return 0;
+}
